464dd00c3266c74061cf8a8d673f10105e69f291
Bharath Seshadri <bharathseshadri@kukubara7.local>
2022-01-23 13:33:07 -0600

    Added code changes to write result set to output file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import buildResultSet from './util/clicks-utils';
 import clicks from './resources/clicks.json';
+import * as fs from 'fs';
 
 const resultSet = buildResultSet(clicks);
+fs.writeFileSync('./resultset.json', JSON.stringify(resultSet), { flag:'w+' });
 
